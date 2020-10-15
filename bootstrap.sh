3730e4fd844a79d254f2ec58ff53f8ce537c8a2d
Christophe Stilmant <Christophe.Stilmant@dfki.de>
2020-10-16 01:29:04 +0200

    Added first basic bootstrap script and manifest

---
+++ bootstrap.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+echo "Bootstraping Pilsbot workspace"
+
+# Get directory of this bootstrap file
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+# Source ROS
+cd $DIR
+source /opt/ros/melodic/setup.bash
+
+# Create directory that will store the git repositories
+mkdir -p src/
+cd src/
+
+# Function to read a package element from manifest.xml
+read_packages () {
+    local IFS=\>
+    read -d \< ENTITY CONTENT
+    local RET=$?
+    TAG_NAME=${ENTITY%% *}
+    ATTRIBUTES=${ENTITY%/}
+    return $RET
+}
+
+# For each package in manifest, clone the repository
+clone_packages () {
+    if [[ $TAG_NAME = "package" ]] ; then
+        eval local $ATTRIBUTES
+        echo "git clone $url -b $branch $name"
+        git clone $url -b $branch $name
+    elif [[ $TAG_NAME = "env" ]] ; then
+        eval local $ATTRIBUTES
+        echo "source $path"
+        source $path
+    fi
+}
+
+while read_packages; do
+    clone_packages
+done < "$DIR/manifest.xml"
+
+echo "Build project..."
+
+# come back to main workspace directory
+cd $DIR
+
+catkin_make
