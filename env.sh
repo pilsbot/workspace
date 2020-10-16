c8986b520c292c58b5ac4614cab85d5351ee059d
Christophe Stilmant <Christophe.Stilmant@dfki.de>
2020-10-16 12:28:58 +0200

    Adapted scripts

---
+++ env.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# Create variable to use for other bash scripts
+export PILSBOT_ROOT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )/"
+
+# Source the ros specific things if the workspace already initialized
+if [[ -f "${PILSBOT_ROOT_DIR}devel/setup.bash" ]]; then
+  source ${PILSBOT_ROOT_DIR}devel/setup.bash
+fi
+
+# Add this bin directory in order to use the pilsbot commands
+export PATH=${PILSBOT_ROOT_DIR}tools/:$PATH
